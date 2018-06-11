c2d3a76fa393d1b5a33a57249d7b34809948277c
Ethan Arrowood <ethan.arrowood@gmail.com>
2018-06-11 18:35:40 +1000

    Add support for multiple composite file names. Unit tests

---
+++ mu1tip1e.composite.test.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const t = require('tap')
+const test = t.test
+const fp = require('./')
+
+test('anonymous function should be named mu1tip1e.composite.test', t => {
+  t.plan(1)
+
+  const fn = fp((fastify, opts, next) => {
+    next()
+  })
+
+  t.is(fn[Symbol.for('fastify.display-name')], 'mu1tip1e.composite.test')
+})
