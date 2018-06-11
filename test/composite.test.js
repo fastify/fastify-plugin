14c8a576aeb3f56db4fd865c5e0db1c11de248ac
Ethan Arrowood <ethan.arrowood@gmail.com>
2018-06-11 18:59:06 +1000

    Update regex to support composite file names (#39)
    * update regex to support composite file names
    
    * Add support for multiple composite file names. Unit tests
    
    * Update npm test script
    
    * Move test files to test directory

---
+++ test/composite.test.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const t = require('tap')
+const test = t.test
+const fp = require('./../')
+
+test('anonymous function should be named composite.test', t => {
+  t.plan(1)
+
+  const fn = fp((fastify, opts, next) => {
+    next()
+  })
+
+  t.is(fn[Symbol.for('fastify.display-name')], 'composite.test')
+})
