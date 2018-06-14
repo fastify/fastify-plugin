2f0cf764eb80f152395fac15dded2baa0359d183
Matteo Collina <hello@matteocollina.com>
2018-06-14 15:23:09 +0200

    Bumped v1.1.3.

---
+++ test/composite.ts
@@ -0,0 +1,14 @@
+import * as fp from './../'
+import * as tap from 'tap'
+
+const test = tap.test
+
+test('anonymous function should be named composite.test', (t: any) => {
+  t.plan(1)
+
+  const fn = fp((fastify, opts, next) => {
+    next()
+  })
+
+  t.is(fn[Symbol.for('fastify.display-name')], 'composite')
+})
