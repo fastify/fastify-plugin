fac8d1a8a335c54923227ec4812cd40f8124e89d
Ethan Arrowood <ethan.arrowood@gmail.com>
2020-08-28 04:47:13 -0400

    fix types and improve tests (#104)

--- plugin.d.ts
+++ plugin.d.ts
@@ -13,23 +13,10 @@ import {
  * @param fn Fastify plugin function
  * @param options Optional plugin options
  */
-export default function fp<Options>(
-  fn: FastifyPluginCallback<Options>,
-  options?: PluginMetadata,
-): FastifyPluginCallback<Options>;
-export default function fp<Options>(
-  fn: FastifyPluginAsync<Options>,
-  options?: PluginMetadata,
-): FastifyPluginAsync<Options>;
-
-export default function fp<Options>(
-  fn: FastifyPluginCallback<Options>,
-  options?: string,
-): FastifyPluginCallback<Options>;
-export default function fp<Options>(
-  fn: FastifyPluginAsync<Options>,
-  options?: string,
-): FastifyPluginAsync<Options>;
+export default function fp<Options>(fn: FastifyPluginAsync<Options>, options?: PluginMetadata): FastifyPluginAsync<Options>;
+export default function fp<Options>(fn: FastifyPluginAsync<Options>, options?: string): FastifyPluginAsync<Options>;
+export default function fp<Options>(fn: FastifyPluginCallback<Options>, options?: PluginMetadata): FastifyPluginCallback<Options>;
+export default function fp<Options>(fn: FastifyPluginCallback<Options>, options?: string): FastifyPluginCallback<Options>;
 
 export interface PluginMetadata {
   /** Bare-minimum version of Fastify for your plugin, just add the semver range that you need. */
@@ -47,5 +34,3 @@ export interface PluginMetadata {
 
 // Exporting PluginOptions for backward compatibility after renaming it to PluginMetadata
 export interface PluginOptions extends PluginMetadata {}
-
-export type NextCallback = (err?: Error) => void;
