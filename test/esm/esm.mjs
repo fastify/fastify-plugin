7b614ef5b1783871d3bffbb74a04e3a64c73a393
Vincent LE GOFF <vince.legoff@gmail.com>
2020-09-15 10:22:56 +0200

    fix: deleting legacy test

--- test/esm/esm.mjs
+++ test/esm/esm.mjs
@@ -14,21 +14,3 @@ t.test('esm base support', async t => {
 
   t.end()
 })
-
-t.test('esm support: should throw if the fastify version does not satisfies the plugin requested version', t => {
-  t.plan(1)
-
-  function plugin (fastify, opts, next) {
-    next()
-  }
-
-  const packageJson = JSON.parse(readFileSync(resolve(dirname(fileURLToPath(import.meta.url)), '../../node_modules/fastify/package.json')))
-
-  const v = packageJson.version.replace(/-(rc|alpha)\.\d+/, '')
-  try {
-    fp(plugin, { fastify: '1000.1000.1000' })
-    t.fail()
-  } catch (e) {
-    t.is(e.message, `fastify-plugin: plugin - expected '1000.1000.1000' fastify version, '${v}' is installed`)
-  }
-})
