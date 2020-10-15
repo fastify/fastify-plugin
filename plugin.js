e056281bf4a6d0681bf707519e8a82150779df07
Vincent LE GOFF <vince.legoff@gmail.com>
2020-10-15 17:39:09 +0200

    feat: delete version testing (#107)
    * feat: delete version testing
    
    * fix: deleting legacy test
    
    * fix: lint
    
    * fix: review

--- plugin.js
+++ plugin.js
@@ -1,21 +1,21 @@
 'use strict'
 
-const semver = require('semver')
-const console = require('console')
 const extractPluginName = require('./stackParser')
-const { join, dirname } = require('path')
 
 let count = 0
 
 function plugin (fn, options = {}) {
   let autoName = false
 
-  if (typeof fn.default !== 'undefined') { // Support for 'export default' behaviour in transpiled ECMAScript module
+  if (typeof fn.default !== 'undefined') {
+    // Support for 'export default' behaviour in transpiled ECMAScript module
     fn = fn.default
   }
 
   if (typeof fn !== 'function') {
-    throw new TypeError(`fastify-plugin expects a function, instead got a '${typeof fn}'`)
+    throw new TypeError(
+      `fastify-plugin expects a function, instead got a '${typeof fn}'`
+    )
   }
 
   fn[Symbol.for('skip-override')] = true
@@ -23,12 +23,21 @@ function plugin (fn, options = {}) {
   const pluginName = (options && options.name) || checkName(fn)
 
   if (typeof options === 'string') {
-    checkVersion(options, pluginName)
-    options = {}
+    options = {
+      fastify: options
+    }
   }
 
-  if (typeof options !== 'object' || Array.isArray(options) || options === null) {
+  if (
+    typeof options !== 'object' ||
+    Array.isArray(options) ||
+    options === null
+  ) {
     throw new TypeError('The options object should be an object')
+  }
+
+  if (options.fastify !== undefined && typeof options.fastify !== 'string') {
+    throw new TypeError(`fastify-plugin expects a version string, instead got '${typeof options.fastify}'`)
   }
 
   if (!options.name) {
@@ -37,11 +46,6 @@ function plugin (fn, options = {}) {
   }
 
   fn[Symbol.for('fastify.display-name')] = options.name
-
-  if (options.fastify) {
-    checkVersion(options.fastify, pluginName)
-  }
-
   fn[Symbol.for('plugin-meta')] = options
 
   // Faux modules support
@@ -75,51 +79,6 @@ function toCamelCase (name) {
     return g1.toUpperCase()
   })
   return newName
-}
-
-function resolvePkgPath (mainFilename) {
-  return join(dirname(require.resolve('fastify', { paths: [mainFilename] })), 'package.json')
-}
-
-function tryGetPath (p) {
-  var pkgPath
-  try {
-    pkgPath = resolvePkgPath(p)
-  } catch (_) {
-  }
-  return pkgPath
-}
-
-function checkVersion (version, pluginName) {
-  if (typeof version !== 'string') {
-    throw new TypeError(`fastify-plugin expects a version string, instead got '${typeof version}'`)
-  }
-
-  // TODO refactor this check and move it inside Fastify itself. https://github.com/fastify/fastify/issues/2507
-  var fastifyVersion
-  var pkgPath
-  if (require.main && require.main.filename) {
-    // We need to dynamically compute this to support yarn pnp
-    pkgPath = tryGetPath(require.main.filename)
-  }
-  if (!pkgPath && process.argv[1]) {
-    // We need this to support native ESM context
-    pkgPath = tryGetPath(process.argv[1])
-  }
-  if (!pkgPath) {
-    // In bundlers, there is no require.main.filename so we go ahead and require directly
-    pkgPath = 'fastify/package.json'
-  }
-
-  try {
-    fastifyVersion = semver.coerce(require(pkgPath).version)
-  } catch (_) {
-    console.info('fastify not found, proceeding anyway')
-  }
-
-  if (fastifyVersion && !semver.satisfies(fastifyVersion, version)) {
-    throw new Error(`fastify-plugin: ${pluginName} - expected '${version}' fastify version, '${fastifyVersion}' is installed`)
-  }
 }
 
 plugin.default = plugin
--- test/esm/esm.mjs
+++ test/esm/esm.mjs
@@ -1,7 +1,4 @@
 import t from 'tap'
-import { readFileSync } from 'fs'
-import { resolve, dirname } from 'path'
-import { fileURLToPath } from 'url'
 
 import fp from '../../plugin.js'
 
@@ -13,22 +10,4 @@ t.test('esm base support', async t => {
   })
 
   t.end()
-})
-
-t.test('esm support: should throw if the fastify version does not satisfies the plugin requested version', t => {
-  t.plan(1)
-
-  function plugin (fastify, opts, next) {
-    next()
-  }
-
-  const packageJson = JSON.parse(readFileSync(resolve(dirname(fileURLToPath(import.meta.url)), '../../node_modules/fastify/package.json')))
-
-  const v = packageJson.version.replace(/-(rc|alpha)\.\d+/, '')
-  try {
-    fp(plugin, { fastify: '1000.1000.1000' })
-    t.fail()
-  } catch (e) {
-    t.is(e.message, `fastify-plugin: plugin - expected '1000.1000.1000' fastify version, '${v}' is installed`)
-  }
 })
